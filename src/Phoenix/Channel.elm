063fc3f0c24231433a6cbfeadba54e6d6a5a8cb1
Kayser <zkayser@gmail.com>
2019-01-29 08:42:16 -0500

    Use EntityState for Channel state

--- src/Phoenix/Channel.elm
+++ src/Phoenix/Channel.elm
@@ -2,6 +2,9 @@ module Phoenix.Channel exposing
     ( Channel
     , init, withPayload, on
     , command, encode
+    , closed
+    , errored
+    , joined
     )
 
 {-| A channel declares a topic to be joined, registers event handlers for the topic, and has various callbacks for lifecycle events.
@@ -63,6 +66,23 @@ init topic =
     , state = Initializing
     }
 
+{-| Sets channel state to connected.
+-}
+joined : Channel msg -> Channel msg
+joined channel =
+    { channel | state = Connected }
+
+{-| Sets channel state to errored.
+-}
+errored : Channel msg -> Channel msg
+errored channel =
+    { channel | state = Errored }
+
+{-| Sets channel state to closed.
+-}
+closed : Channel msg -> Channel msg
+closed channel =
+    { channel | state = Closed }
 
 {-| Attaches a payload to the join message. This should be used to submit user IDs, authentication credentials, etc. The payload will
 be received as the second argument to the `join/3` callback on the server.
